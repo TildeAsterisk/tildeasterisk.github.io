e5a32e7d4145ef013937994081b3220267df224a
~* <64159585+TildeAsterisk@users.noreply.github.com>
2022-12-16 01:06:21 +0000

    Updating out of money alert

--- ASCII-Clicker/script.js
+++ ASCII-Clicker/script.js
@@ -147,7 +147,7 @@ function Clicker(){
     }
     else{
       //Not enough money
-      alert("You're too broke for that lol.");
+      alert("That costs: "+shopItem[1]+". You have: "+score.toFixed()+". You're too broke for that lol.");
       GenerateShopItem()
     }
   };
