8ad47eb8375cbb0580e5c02cc857fa3e75176780
~* <64159585+TildeAsterisk@users.noreply.github.com>
2023-12-09 03:01:12 +0000

    sync with 000webhost and init git-ftp to webhost server

--- connection.php
+++ connection.php
@@ -1,8 +1,7 @@
 <?php 
-$mysql = mysqli_connect("localhost", "root", "Cxbolloz99", "gamedb");
-//$mysql = mysqli_connect("localhost", "root", "Cxbolloz99", "gamedb");
+$mysql = mysqli_connect("localhost", "id21643467_tildeasterisk", "Dbbolloz99!", "id21643467_gamedb");
 if ($mysql->connect_errno) {
     die("Failed to connect to MySQL: " . $mysql->connect_error);
 }
-mysqli_select_db($mysql,"gamedb");
+mysqli_select_db($mysql,"id21643467_gamedb");
 ?>
