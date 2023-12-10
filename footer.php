23bb04da3817d1aeec9e469b9111e0eb64468b91
Tilde Asterisk <tildeAsterisk@gmail.com>
2023-12-10 07:08:17 +0000

    Updating...

--- footer.php
+++ footer.php
@@ -2,8 +2,14 @@
     </div>
   <div id="footer">Copyright &copy; 2023 TildeAsterisk ~*</div> 
 
+<div style="position: fixed; bottom: 0px; left: 10px; font-size: 12px;">
+  <br><br><br><br>
+  <a target="_blank" href="https://tildeasterisk.blogspot.com/p/home.html">Blog</a> - 
+  <a target="_blank" href="https://tildeasterisk.blogspot.com/p/home.html">Other Games</a>
+  <br><br>
+</div>
+
   <div id="ASCIIAnimDiv"><pre>ascii anim hjere</pre></div>
       <script src="client.js"></script>
-
 </body>
 </html>
